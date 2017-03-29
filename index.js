ba9409d88bfa2b4a1033e10133e65a9afaa77226
Hamish Willee <hamishwillee@gmail.com>
2017-03-29 21:23:47 +1100

    test

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ module.exports = {
               var $ = cheerio.load(page.content);
               var matchthingy =  $('ul.summary > li.active').children().text();
               console.log(matchthingy);
+              // Write a file in the output folder
+              output.writeFile('hello.txt', matchthingy);
             
-              $('.search-noresults').append('<p>THIS IS SOME RANDOM TEXT</p>');
+              //$('.search-noresults').append('<p>THIS IS SOME RANDOM TEXT</p>');
             
 
               page.content = $.html()
